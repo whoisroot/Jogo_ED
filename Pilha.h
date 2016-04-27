9d948f739de980e44aa3254af96c6b961ab21a61
Lucas Magalhães <lucassmagalhaes@uol.com.br>
2016-04-27 01:17:02 -0300

    Mais imagens, botões que funcionam e melhorias no codigo

---
+++ Pilha.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include "Hamburguer.h"
+
+using namespace std;
+
+typedef struct No{
+    int ingrediente;
+    No *abaixo;
+    No *acima;
+}No;
+
+class Pilha{
+private:
+    No *Topo;
+    No *Base;
+    No *Atual;
+    int tam;
+public:
+    Pilha();
+    bool Inserir(int x);
+    bool Remover(int x);
+    bool Vazia();
+    bool Cheia();
+    int Percorre();
+}
